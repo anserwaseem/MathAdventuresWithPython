346b79defa4292b6afcbfc713ba98fc76555468e
Anser Waseem <hafiz.anser.waseem@gmail.com>
2020-06-23 21:49:14 +0500

    Add files via upload

---
+++ ex2.1FindingTheSum.py
@@ -0,0 +1,10 @@
+# Find the sum of l numbers from 1 to a given number.
+
+form turtle import *
+shape('turtle')
+
+def FindSum(maxLimit=100):
+    summ=0
+    for i in range(maxLimit):
+        summ+=(i+1)
+    return summ
